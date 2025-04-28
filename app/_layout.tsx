4951ba0eb79d8baaf65014e07bd5a8bfb328366d
Cakhr <cgwilliams3@wpi.edu>
2025-04-28 17:33:30 -0400

    push2

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
-import { Stack } from "expo-router";
-import { verifyInstallation } from "nativewind";
-import "../app/global.css";
+import { Stack } from 'expo-router';
+import { verifyInstallation } from 'nativewind';
+import '../app/global.css';
 
 export default function RootLayout() {
 	verifyInstallation();
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,19 @@
-import { Text, View, Button } from "react-native";
+import { Text, View, Button } from 'react-native';
 
 export default function Index() {
 	return (
 		<View
 			style={{
 				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
+				justifyContent: 'center',
+				alignItems: 'center'
 			}}
 		>
-			<Text className={"bg-red-500 p-2 rounded-lg"}>
+			<Text className={'bg-red-500 p-2 rounded-lg'}>
 				Edit app/index.tsx to edit this screen.
 			</Text>
-			<div className={"m-2 bg-black text-white p-2 rounded-full"}> 
-				{"Hihi"}
+			<div className={'m-2 bg-black text-white p-2 rounded-full'}>
+				{'Hihi'}
 			</div>
 		</View>
 	);
