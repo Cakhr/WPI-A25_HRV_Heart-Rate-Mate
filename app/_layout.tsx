9675833637392589d3795ea964fdc3dcc224f869
Cakhr <cgwilliams3@wpi.edu>
2025-09-02 22:06:51 +0000

    Expo-SQLite & Drizzle

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,62 @@
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import '../app/global.css';
 import { BottomNavBar } from '@/components/BottomNavBar';
+import { useDrizzleStudio } from 'expo-drizzle-studio-plugin';
+
+import * as SQLite from 'expo-sqlite';
+import { useEffect, useState } from 'react';
+import { drizzle } from 'drizzle-orm/expo-sqlite';
+import { usersTable } from '../db/schema';
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
+import migrations from '../drizzle/migrations';
+
+const expo = SQLite.openDatabaseSync('db.db');
+const db = drizzle(expo);
 
 export default function RootLayout() {
+  useDrizzleStudio(expo);
+  const { success, error } = useMigrations(db, migrations);
+  const [items, setItems] = useState<(typeof usersTable.$inferSelect)[] | null>(
+    null
+  );
+  useEffect(() => {
+    if (!success) return;
+    (async () => {
+      await db.delete(usersTable);
+      await db.insert(usersTable).values([
+        {
+          name: 'John',
+          age: 30,
+          email: 'john@example.com'
+        }
+      ]);
+      const users = await db.select().from(usersTable);
+      setItems(users);
+    })();
+  }, [success]);
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+  if (!success) {
+    return (
+      <View>
+        <Text>Migration is in progress...</Text>
+      </View>
+    );
+  }
+  if (items === null || items.length === 0) {
+    return (
+      <View>
+        <Text>Empty</Text>
+      </View>
+    );
+  }
+
   return (
     <View className={'size-full'}>
       <View className={'absolute bottom-0 left-0 z-50'}>
